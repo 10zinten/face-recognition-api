19cc40f49dff6eee6a1acb03df636d23a4b7a371
tenzin <ten13zin@gmail.com>
2019-03-17 16:38:55 +0530

    add auth RestFul API endpoint

--- app/api.py
+++ app/api.py
@@ -39,5 +39,42 @@ def register():
     return Response(response_pickled, status =200, mimetype="application/json")
 
 
+def is_authenticate(userid, file_obj):
+    user_dir = os.path.join('auth', userid)
+    src_encoding_fn = os.path.join(user_dir, '{}_encoding.npy'.format(userid))
+    src_encoding = np.load(src_encoding_fn)
+
+    img = face_recognition.load_image_file(file_obj)
+    encoding = face_recognition.face_encodings(img)[0]
+
+    result = face_recognition.compare_faces([src_encoding], encoding)[0]
+
+    return bool(result)
+
+
+@app.route("/auth", methods=["POST"])
+def authenticate():
+    if request.method == "POST":
+        file_obj = request.files['image']
+        userid = request.form['userid']
+
+        if file_obj and userid:
+            response = {
+                'upload_data': True,
+                'userid': userid,
+                'auth_status': is_authenticate(userid, file_obj),
+            }
+        else:
+            response = {
+                'upload_data': False,
+                'userid': userid,
+                'auth_status': False
+            }
+
+        response_pickled = jsonpickle.encode(response)
+
+        return Response(response_pickled, status =200, mimetype="application/json")
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
